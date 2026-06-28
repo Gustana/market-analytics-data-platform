4f10c44b3ce1711f64714bacf5227549f63d8920
Whyan <work.gustana@gmail.com>
2026-06-28 18:26:32 +0700

    Refactor/directory/schema/marketstack/change directory structure (#6)
    * refactor: update marketstack directory structures
    
    * refactor: rename class for clarity

--- src/extract/schemas/responses/common_response.py
+++ src/extract/schemas/responses/common_response.py
@@ -1,11 +1,11 @@
 from pydantic import BaseModel
 
-class Paggination(BaseModel):
+class PaginationRecords(BaseModel):
     limit: int
     offset: int
     count: int
     total: int
 
-class FixerErrorRecord(BaseModel):
+class ErrorRecords(BaseModel):
     code: int
     info: str
