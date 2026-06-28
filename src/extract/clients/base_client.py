c6ce734aeb0092e7004d2bbb0c307ed707f0b6d4
Whyan <work.gustana@gmail.com>
2026-06-28 17:46:50 +0700

    Feat/extract/api client/enforce validity of client params (#3)
    * refactor: remove fixer client
    
    * feat: enforce validity of params on extract api clients

--- src/extract/clients/base_client.py
+++ src/extract/clients/base_client.py
@@ -1,12 +1,13 @@
-from typing import TypeVar
+from typing import Any, TypeVar
 
 import httpx
-from pydantic import BaseModel
+from pydantic import TypeAdapter
 
+from src.extract.clients.utils import ClientUtils
 from src.extract.utils.constants import Constants
 from src.extract.utils.logger import get_logger
 
-ResponseTypeT = TypeVar('ResponseTypeT', bound=BaseModel)
+ResponseTypeT = TypeVar('ResponseTypeT')
 
 class BaseClient:
     def __init__(self, base_url: str):
@@ -17,17 +18,30 @@ class BaseClient:
             pool=Constants.POOL_CONNECTION_TIMEOUT_IN_SECOND
         )
 
+        if not ClientUtils.is_url_valid(base_url):
+            raise ValueError(f'invalid base_url: {base_url}')
+
         self._client = httpx.Client(base_url=base_url, timeout=_timeout_config)
         self._logger = get_logger(default_context={"base_url": base_url})
 
-    def get(self, endpoint: str, params: dict, response_model: type[ResponseTypeT]) -> ResponseTypeT:
+    def get(self, endpoint: str, request_param: dict[str, Any], response_model: type[ResponseTypeT]) -> ResponseTypeT:
+        if type(endpoint) != str:
+            raise TypeError(f'endpoint type must be string, passed type: {type(endpoint)}')
+        
+        if endpoint.split() == '' or '/' not in endpoint:
+            raise ValueError(f'endpoint value invalid: {endpoint}')
+        
+        if type(request_param) != dict:
+            raise TypeError(f'request_param must be dict type, passed type: {type(endpoint)}')
+        
         try:
-            response = self._client.get(endpoint, params=params)
+            response = self._client.get(endpoint, params=request_param)
             response.raise_for_status()
 
-            return response_model.model_validate(response.json())
-        except Exception as e:
+            return TypeAdapter(response_model).validate_python(response.json())
+        except httpx.HTTPError as e:
             self._logger.error(f'Error occurred when calling the api {e}')
+            raise
     
     def close(self):
         self._client.close()
