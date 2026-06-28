de75b13e8f34b3adb95afc2a007d6ceea8382fee
Whyan <work.gustana@gmail.com>
2026-06-28 18:22:00 +0700

    Refactor/extract/client/remove fixer extract api client (#5)
    * refactor: delete fixer client params
    
    * refactor: delete fixer responses schemas

--- src/extract/schemas/params/fixer/supported_currency_codes.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, ConfigDict
-
-class SupportedCurrencyCodesParam(BaseModel):
-    # update this model if the api needs parameters
-
-    model_config = ConfigDict(populate_by_name=True)
